d2bd22f6dccf4ca06fbc6b887ca9f2f1d2d5d5f4
msomea <msomearaphael@gmail.com>
2023-04-20 02:44:46 +0300

    Add function for first two round play

--- main.py
+++ main.py
@@ -43,7 +43,59 @@ def dealer_hand_selection():
     dealer_hand_5 = random.choice(deck)
     dealer_hand_6 = random.choice(deck)
 
-
+def first_rounds():
+    global player_total, player_hand_count, hit_choice, win_condition, player_blackjack, player_bust, dealer_blackjack, tie_game
+    win_condition = 0
+    player_bust = 0
+    player_blackjack = 0
+    dealer_blackjack = 0
+    tie_game = 0
+    if player_hand_count == 1:
+        player_hand_count = 2
+        print('***********************************************************')
+        print('')
+        print(f' New Game: {player_name.title()}  VS Computer Dealer')
+        print(" ")
+        player_total = player_hand_1
+        print('***********************************************************')
+        print(' First Dealing')
+        time.sleep(0.5)
+        print(' Dealer is Dealing')
+        time.sleep(0.5)
+        print(' Please wait...')
+        wait = ('*****')
+        print(f' {wait[:1]}')
+        time.sleep(0.5)
+        print(f' {player_name.title()} has {player_hand_1} on hand')
+        time.sleep(0.5)
+        print(f' Dealer has {dealer_hand_1} on hand')
+        print(' ')
+        
+    if player_hand_count == 2:
+        player_total = player_hand_1 + player_hand_2
+        print('***********************************************************')
+        print(' Second Dealing')
+        time.sleep(0.5)
+        print(' Dealer is Dealing')
+        time.sleep(0.5)
+        print(' Please wait...')
+        wait = ('****')
+        print(f' {wait[:1]}')
+        time.sleep(0.5)
+        print(f' {wait[:2]}')
+        time.sleep(0.5)
+        print(f' {player_name.title()} has {player_hand_1} and {player_hand_2} on hand')
+        time.sleep(0.5)
+        print(f' Dealer has {dealer_hand_1} and XXX on hand')
+        time.sleep(0.5)
+        print('***********************************************************')
+        print(f' Total {player_name.title()} card is {player_total}')
+        print(f' Total Dealer card is {dealer_hand_1} + XXX ')
+        print('***********************************************************')
+    #Check if player bust
+    
+    #Check if player win BlackJack
+    
 
 
 def main():
