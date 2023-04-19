55a3f10bf14d336eb8d04a529d3ac7026cf03c19
msomea <msomearaphael@gmail.com>
2023-04-20 02:35:57 +0300

    Add functions for getting player name random select dealer and player cards and main function for game playing

--- main.py
+++ main.py
@@ -11,4 +11,48 @@ player_stand = 's'
 player_proceed = 'n'
 player_quite = 'q'
 ask_help = 'help'
-deck = [2, 2]
+deck = [1, 2, 3, 4, 5 ,6, 7, 8, 8, 10, 11, 12, 13]
+
+def get_player_name():
+    global player_name, player_hand_count
+    player_name = input(' Enter your name please:  ')
+    player_name = player_name.strip()
+    #what if Player dont provide a name. Call them Strange Player
+    if player_name == '':
+        player_name = 'Strange Player'
+        print('***********************************************************')
+        print(f' I will call you {player_name}')
+    player_hand_count = 1
+    time.sleep(0.5)
+
+def player_hand_selection():
+    global player_hand_1, player_hand_2, player_hand_3, player_hand_4, player_hand_5, player_hand_6
+    player_hand_1 = random.choice(deck)
+    player_hand_2 = random.choice(deck)
+    player_hand_3 = random.choice(deck)
+    player_hand_4 = random.choice(deck)
+    player_hand_5 = random.choice(deck)
+    player_hand_6 = random.choice(deck)
+    
+def dealer_hand_selection():
+    global dealer_hand_1, dealer_hand_2, dealer_hand_3, dealer_hand_4, dealer_hand_5, dealer_hand_6
+    dealer_hand_1 = random.choice(deck)
+    dealer_hand_2 = random.choice(deck)
+    dealer_hand_3 = random.choice(deck)
+    dealer_hand_4 = random.choice(deck)
+    dealer_hand_5 = random.choice(deck)
+    dealer_hand_6 = random.choice(deck)
+
+
+
+
+def main():
+    global player_name
+    #Get player name
+    get_player_name()
+    #Player card random selection from deck
+    player_hand_selection()
+    #Dealer card random selection from deck
+    dealer_hand_selection()
+main()   
+      
